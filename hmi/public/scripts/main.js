b093f259d5929eb0a121b60d9a3ff3ae602c4456
Feliks <feliks.montez@gmail.com>
2019-12-04 18:07:39 -0500

    add bootstrap/knockstrap

--- hmi/public/scripts/main.js
+++ hmi/public/scripts/main.js
@@ -2,11 +2,21 @@ requirejs.config({
     paths: {
         jquery: 'lib/jquery.min',
         knockout: 'lib/knockout.min',
+        knockstrap: 'lib/knockstrap.min',
+        bootstrap: 'lib/bootstrap-3.4.1/bootstrap.min',
         text: 'lib/text'
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        },
+        knockstrap: {
+            deps: ['knockout', 'jquery', 'bootstrap']
+        }
     }
 });
 
-requirejs(['jquery', 'knockout', '../components/all'], function($, ko) {
+requirejs(['jquery', 'knockout', 'knockstrap', 'bootstrap', '../components/all'], function($, ko) {
     $.get("http://localhost:5900/info", {}, function(data) {
         console.log(data);
     });
