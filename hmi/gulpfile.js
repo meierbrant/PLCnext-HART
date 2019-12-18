ddf037445faed9c25e280780af8c51b8ae91e983
Feliks <feliks.montez@gmail.com>
2019-12-18 11:17:44 -0500

    gulp

--- hmi/gulpfile.js
+++ hmi/gulpfile.js
@@ -4,9 +4,17 @@ var fs = require('fs'), vm = require('vm'), merge = require('deeply'), chalk = r
 // Gulp and plugins
 var gulp = require('gulp'), rjs = require('gulp-requirejs-bundler'), concat = require('gulp-concat'), clean = require('gulp-clean'), filter = require('gulp-filter'),
     replace = require('gulp-replace'), uglify = require('gulp-uglify'), htmlreplace = require('gulp-html-replace'),
-    connect = require('gulp-connect'), babelCore = require('babel-core'), babel = require('gulp-babel'), objectAssign = require('object-assign');
+    connect = require('gulp-connect'), babelCore = require('babel-core'), babel = require('gulp-babel'), objectAssign = require('object-assign'),
+    sass = require('gulp-sass'),
+    // ts = require('gulp-typescript'),
+    newer = require('gulp-newer'),
+    imagemin = require('gulp-imagemin');
 
+const
+    src = './src/',
+    build = './dist/';
 // Config
+sass.compiler = require('node-sass');
 var requireJsRuntimeConfig = vm.runInNewContext(fs.readFileSync('src/app/require.config.js') + '; require;'),
     requireJsOptimizerConfig = merge(requireJsRuntimeConfig, {
         out: 'scripts.js',
@@ -41,6 +49,14 @@ var requireJsRuntimeConfig = vm.runInNewContext(fs.readFileSync('src/app/require
         return babelCore.util.regexify(item);
     });
 
+
+// const tsProject = ts.createProject('tsconfig.json');
+// gulp.task('ts', function() {
+//     return gulp.src(src + '**/*.ts')
+//         .pipe(tsProject()).js
+//         .pipe(gulp.dest(build));
+// });
+
 // Pushes all the source files through Babel for transpilation
 gulp.task('js:babel', function() {
     return gulp.src(requireJsOptimizerConfig.baseUrl + '/**')
@@ -64,7 +80,7 @@ gulp.task('js:optimize', ['js:babel'], function() {
     var config = objectAssign({}, requireJsOptimizerConfig, { baseUrl: 'temp' });
     return rjs(config)
         .pipe(uglify({ preserveComments: 'some' }))
-        .pipe(gulp.dest('./dist/'));    
+        .pipe(gulp.dest(build));    
 })
 
 // Builds the distributable .js files by calling Babel then the r.js optimizer
@@ -81,7 +97,21 @@ gulp.task('css', function () {
         combinedCss = es.concat(bowerCss, appCss).pipe(concat('css.css')),
         fontFiles = gulp.src('./src/bower_modules/components-bootstrap/fonts/*', { base: './src/bower_modules/components-bootstrap/' });
     return es.concat(combinedCss, fontFiles)
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest(build));
+});
+
+gulp.task('sass:src', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('images', function() {
+    const out = build + 'img/';
+    return gulp.src(src + 'img/**/*')
+        .pipe(newer(out))
+        .pipe(imagemin({ optimizationLevel: 5 }))
+        .pipe(gulp.dest(out));
 });
 
 // Copies index.html, replacing <script> and <link> tags to reference production URLs
@@ -91,22 +121,23 @@ gulp.task('html', function() {
             'css': 'css.css',
             'js': 'scripts.js'
         }))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest(build));
 });
 
 // Removes all files from ./dist/
 gulp.task('clean', function() {
-    return gulp.src('./dist/**/*', { read: false })
+    return gulp.src(build + '**/*', { read: false })
         .pipe(clean());
 });
 
 // Starts a simple static file server that transpiles ES6 on the fly to ES5
-gulp.task('serve:src', function() {
+gulp.task('serve:src', ['sass:src'], function() {
     return connect.server({
         root: transpilationConfig.root,
         middleware: function(connect, opt) {
             return [
-                 function (req, res, next) {                     
+                 function (req, res, next) {       
+                     console.log("req: ", req)              
                      var pathname = path.normalize(url.parse(req.url).pathname);
                      babelTranspile(pathname, function(err, result) {
                         if (err) {
@@ -137,7 +168,7 @@ function babelTranspile(pathname, callback) {
     babelCore.transformFile(src, opts, callback);
 }
 
-gulp.task('default', ['html', 'js', 'css'], function(callback) {
+gulp.task('default', ['html', 'js', 'css', 'images'], function(callback) {
     callback();
-    console.log('\nPlaced optimized files in ' + chalk.magenta('dist/\n'));
+    console.log('\nPlaced optimized files in ' + chalk.magenta(build + '\n'));
 });
--- hmi/src/app/startup.js
+++ hmi/src/app/startup.js
@@ -7,6 +7,7 @@ import * as router from './router';
 // Components can be packaged as AMD modules, such as the following:
 ko.components.register('nav-bar', { require: 'components/nav-bar/nav-bar' });
 ko.components.register('home-page', { require: 'components/home-page/home' });
+ko.components.register('hart-device', { require: 'components/hart-device/hart-device' });
 
 // ... or for template-only components, you can just point to a .html file directly:
 ko.components.register('about-page', {
